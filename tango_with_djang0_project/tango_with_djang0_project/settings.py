348a706e63eaa430d413c75d61d561bb69b0d9a9
kojo <kojo_caesar@yahoo.com>
2014-07-28 22:08:50 +0300

    first template

--- tango_with_djang0_project/tango_with_djang0_project/settings.py
+++ tango_with_djang0_project/tango_with_djang0_project/settings.py
@@ -1,5 +1,9 @@
 # Django settings for tango_with_djang0_project project.
-
+import os
+SETTINGS_DIR = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -72,6 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
 )
 
 # List of finder classes that know how to find static files in
@@ -89,7 +94,8 @@ SECRET_KEY = 'm_)5r8ma+9js(-0jo^atf*r1&wm35c4l6(ih+)6ek!!3kz498g'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+#     'django.template.loaders.eggs.Loader', 
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -111,6 +117,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
